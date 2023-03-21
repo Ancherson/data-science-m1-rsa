bda683362e23a75e4984de127db472ed9e755202
Ancherson <billy@BillyTheKid>
2023-03-21 17:25:20 +0100

    debut de la sauce maison

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ spark = SparkSession.builder.master("local[*]").getOrCreate()
 
 df_x = pd.read_csv("./InputTrain.csv")
 df_y = pd.read_csv("./StepOne_LabelTrain.csv")
+merged = pd.merge(df_x.drop(columns=["House_id"]),df_y.drop(columns=["House_id"]),on="Index",how="inner")
+merged = merged.drop(columns = ["Index"])
+
+prossecing = merged.groupby(["Washing Machine","Dishwasher","Tumble Dryer","Microwave","Kettle"])
+print(prossecing.describe())
+print(prossecing.max())
+print(prossecing.min())
+print(prossecing.mean())
