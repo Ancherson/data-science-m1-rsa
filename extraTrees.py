a498e4b97633d2e14edae7590e51a22940f943b8
Alaia <alaia.silvestre1@gmail.com>
2023-03-25 00:53:26 +0100

    extraTrees pour un extra temps de compilation

--- extraTrees.py
+++ extraTrees.py
@@ -1,13 +1,12 @@
 import pandas as pd
 from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.model_selection import GridSearchCV
 
 # load data
 consommation1_df = pd.read_csv("InputTrain.csv")
 consommation2_df = pd.read_csv("InputTest.csv")
 activation_df = pd.read_csv("StepOne_LabelTrain.csv")
 
-# select columns corresponding to appliances in the activation file
+# select columns corresponding to the appliances in the activation file
 appareils = ['Washing Machine', 'Dishwasher', 'Tumble Dryer', 'Microwave', 'Kettle']
 x = consommation1_df.values
 
@@ -19,15 +18,10 @@ for appareil in appareils:
     print(appareil)
     y = activation_df[appareil].values
     et = ExtraTreesClassifier(random_state=0)
-    param_grid = {
-        'n_estimators': [100, 250, 500, 750, 1000, 1500]  # specify values to search
-    }
-    grid_search = GridSearchCV(et, param_grid, cv=5)  # use 5-fold cross validation
-    grid_search.fit(x, y)
-    print(f"Best parameters for {appareil}: {grid_search.best_params_}")
-    resultats[appareil] = grid_search.predict(consommation2_df)
+    et.fit(x, y)
+    resultats[appareil] = et.predict(consommation2_df)
 
 # convert the results to a pandas DataFrame and save to a CSV file
 print("saving results")
 resultats_df = pd.DataFrame(resultats)
-resultats_df.to_csv("etResultsGridSearch.csv", index=True, index_label='Index', columns=appareils)
+resultats_df.to_csv("etResults.csv", index=True, index_label='Index', columns=appareils)
