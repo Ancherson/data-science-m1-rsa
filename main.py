8e77baae97886aef30baedeff599acc10c9edc69
Mathéo DESSAAUVAGES <matdessauv@gmail.com>
2023-03-24 13:10:51 +0100

    Autre solution

--- main.py
+++ main.py
@@ -1,45 +1,64 @@
-import pandas as pd
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
-from pyspark.ml import Pipeline
-from pyspark.ml.feature import VectorAssembler,StandardScaler
 from pyspark.ml.classification import GBTClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.feature import VectorAssembler
+from pyspark.sql.functions import col
+from pyspark.ml.linalg import Vectors
 
-spark = SparkSession.builder.master("local[*]").getOrCreate()
+spark = SparkSession.builder.master("local[*]").config("spark.driver.memory", "6g").getOrCreate()
 
-input_train_df = spark.read.csv("./InputTrain.csv",header=True,inferSchema=True)
-labels_df = spark.read.csv("./StepOne_LabelTrain.csv",header=True,inferSchema=True)
+# Load the training data
+train_data = spark.read.csv("InputTrain.csv", header=True, inferSchema=True)
 
-merged_df = input_train_df.join(labels_df, on="Index",how="inner")
+# Load the labels for the training data
+train_labels = spark.read.csv("StepOne_LabelTrain.csv", header=True, inferSchema=True)
 
-# Define the feature columns and the target column
-feature_cols = input_train_df.columns[2:]  # the first two columns are id and house id
-target_col = "Washing Machine"
-label_cols = ["Washing Machine", "Dishwasher", "Tumble Dryer", "Microwave", "Kettle"]
+# Join the training data with the labels
+train_data = train_data.join(train_labels, "Index")
 
-# Assemble the features into a vector
+# Extract the features from the training data
+feature_cols = [c for c in train_data.columns if c not in ["Index", "House_id", "Washing Machine", "Dishwasher", "Tumble Dryer", "Microwave", "Kettle"]]
 assembler = VectorAssembler(inputCols=feature_cols, outputCol="features")
-data = assembler.transform(merged_df).select(col("features"), *label_cols)
+train_data = assembler.transform(train_data).select("features", "Washing Machine", "Dishwasher", "Tumble Dryer", "Microwave", "Kettle")
 
-# Split the data into training and validation sets
-(training_data, validation_data) = data.randomSplit([0.8, 0.2], seed=42)
+# Train a GBT classifier for each appliance
+models = {}
+evaluator = MulticlassClassificationEvaluator(metricName="weightedPrecision")
+for appliance in ["Washing Machine", "Dishwasher", "Tumble Dryer", "Microwave", "Kettle"]:
+    train_data = train_data.withColumnRenamed(f"{appliance}_consumption", appliance)
+    gbt = GBTClassifier(labelCol=appliance)
+    model = gbt.fit(train_data)
+    models[appliance] = model
 
-# Train the GBT model
-gbt = GBTClassifier(labelCol=target_col, featuresCol="features", maxIter=10)
-model = gbt.fit(training_data)
+    # Evaluate the model on the training data
+    predictions = model.transform(train_data)
+    predictions = predictions.withColumn("label", col(appliance))
+    precision = evaluator.evaluate(predictions)
+    print(f"{appliance} model precision: {precision}")
 
-# Evaluate the model on the validation set
-evaluator = MulticlassClassificationEvaluator(labelCol=target_col, predictionCol="prediction", metricName="accuracy")
-predictions = model.transform(validation_data)
-accuracy = evaluator.evaluate(predictions)
-print("Accuracy on validation set = %g" % accuracy)
-
-# Use the model to predict the state of the 5 appliances on the test data
+# Load the test data
 test_data = spark.read.csv("InputTest.csv", header=True, inferSchema=True)
-test_features = assembler.transform(test_data).select(col("features"))
-test_features.printSchema()
-test_predictions = model.transform(test_features).select(col("prediction"), col("Washing Machine"), col("Dishwasher"), col("Tumble Dryer"), col("Microwave"), col("Kettle"))
 
-# Save the predictions in the format specified in the instructions
-test_predictions.write.csv("StepOne_Predictions.csv", header=True)
+# Extract the features from the test data
+test_data = assembler.transform(test_data).select("Index", "House_id", "features")
+
+# Make predictions for each appliance and generate the binary vectors
+binary_vectors = []
+for row in test_data.collect():
+    index = row.Index
+    house_id = row.House_id
+    features = row.features
+    feature_array = features.toArray() # Convert to NumPy array
+    feature_list = feature_array.tolist()
+    feature_df = spark.createDataFrame([(Vectors.dense(feature_array),)], ["features"])
+    binary_vector = [index, house_id]
+    for appliance in ["Washing Machine", "Dishwasher", "Tumble Dryer", "Microwave", "Kettle"]:
+        model = models[appliance]
+        prediction = model.transform(feature_df).select("prediction").collect()[0][0]
+        binary_vector.append(int(prediction))
+    binary_vectors.append(binary_vector)
+
+# Save the binary vectors to a CSV file
+binary_vectors_df = spark.createDataFrame(binary_vectors, ["Index", "House_id", "Washing Machine", "Dishwasher", "Tumble Dryer", "Microwave", "Kettle"])
+binary_vectors_df.drop("House_id")
+binary_vectors_df.write.csv("StepOne_Predictions.csv", header=True, mode="overwrite")
