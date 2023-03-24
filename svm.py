8565370e3b881901bbf3baf4672a2daaa3702956
Alaia <alaia.silvestre1@gmail.com>
2023-03-24 18:31:50 +0100

    Sa Va Mieu quand on change le kernel

--- svm.py
+++ svm.py
@@ -1,27 +1,27 @@
 import pandas as pd
 from sklearn.svm import SVC
 
-# Charger les fichiers CSV contenant les données de consommation et d'activation
+# load data
 consommation1_df = pd.read_csv("InputTrain.csv")
 consommation2_df = pd.read_csv("InputTest.csv")
 activation_df = pd.read_csv("StepOne_LabelTrain.csv")
 
-# Sélectionner les colonnes correspondant aux appareils dans le fichier d'activation
+# select columns corresponding to appliances in the activation file
 appareils = ['Washing Machine', 'Dishwasher', 'Tumble Dryer', 'Microwave', 'Kettle']
 x = consommation1_df.values
 
-# Initialiser un dictionnaire pour stocker les résultats de la classification
+# initialize a dictionary to store the classification results
 resultats = ({appareil: [] for appareil in appareils})
 
-# Boucler sur chaque appareil et entraîner un classificateur binaire pour prédire son activation
+# loop over each appliance and train a classifier to predict its activation
 for appareil in appareils:
     print(appareil)
     y = activation_df[appareil].values
-    clf = SVC(kernel='linear', random_state=0) #kernel=sigmoid > poly > rbf, linear
+    clf = SVC(kernel='linear', random_state=0) #kernel=sigmoid > poly > rbf, linear too long
     clf.fit(x, y)
     resultats[appareil] = clf.predict(consommation2_df)
 
-# Convertir les résultats en un DataFrame pandas et les enregistrer dans un fichier CSV
+# convert the results to a pandas DataFrame and save to a CSV file
 print("saving results")
 resultats_df = pd.DataFrame(resultats)
 resultats_df.to_csv("svmResultsLinear.csv", index=True, index_label='Index', columns=appareils)
